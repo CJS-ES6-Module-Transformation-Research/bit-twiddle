dcfd7e67c74c9bfc7fdf4bcedf2bd3bf83bf3631
sam raphael <samraphael@gmail.com>
2021-08-06 14:52:31 -0400

    pre-processing with update

--- test/interleave2.js
+++ test/interleave2.js
@@ -1,13 +1,12 @@
-var test = require("tape")
-  , bits = require("../twiddle.js");
-
-test("interleave2", function(t) {
-  for(var x=0; x<100; ++x) {
-    for(var y=0; y<100; ++y) {
-      var h = bits.interleave2(x, y);
-     t.equal(bits.deinterleave2(h, 0), x);
-     t.equal(bits.deinterleave2(h, 1), y);
+var test = require('tape');
+var bits = require('../twiddle.js');
+test('interleave2', function (t) {
+    for (var x = 0; x < 100; ++x) {
+        for (var y = 0; y < 100; ++y) {
+            var h = bits.interleave2(x, y);
+            t.equal(bits.deinterleave2(h, 0), x);
+            t.equal(bits.deinterleave2(h, 1), y);
+        }
     }
-  }
-  t.end();
+    t.end();
 });
