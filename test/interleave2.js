98cf8be4aa028acfef915033a5ba9095a16ec74b
sam raphael <samraphael@gmail.com>
2021-08-06 14:15:19 -0400

    transformation done

--- test/interleave2.js
+++ test/interleave2.js
@@ -1,11 +1,14 @@
-var test = require('tape');
-var bits = require('../twiddle.js');
+import test from 'tape';
+import {
+    interleave2,
+    deinterleave2
+} from '../twiddle.js';
 test('interleave2', function (t) {
     for (var x = 0; x < 100; ++x) {
         for (var y = 0; y < 100; ++y) {
-            var h = bits.interleave2(x, y);
-            t.equal(bits.deinterleave2(h, 0), x);
-            t.equal(bits.deinterleave2(h, 1), y);
+            var h = interleave2(x, y);
+            t.equal(deinterleave2(h, 0), x);
+            t.equal(deinterleave2(h, 1), y);
         }
     }
     t.end();
